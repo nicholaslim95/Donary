1476fcf0faa8dbd63b9719aee7577cfd6bacd573
Tenk <tenkeason95@gmail.com>
2019-06-09 22:26:42 +0800

    My part

---
+++ app/src/main/java/com/example/donary/AddDonationActivity.java
@@ -0,0 +1,156 @@
+package com.example.donary;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+
+public class AddDonationActivity extends AppCompatActivity {
+
+    Uri imageUri;
+    String myUrl = "";
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    ImageView back, image_added;
+    TextView donate;
+    EditText Title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donation);
+
+        back = findViewById(R.id.back);
+        image_added = findViewById(R.id.image_added);
+        donate = findViewById(R.id.donate);
+        Title = findViewById(R.id.Title);
+
+        storageReference = FirebaseStorage.getInstance().getReference("donates");
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDonationActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        CropImage.activity()
+                .setAspectRatio(1,1)
+                .start(AddDonationActivity.this);
+
+    }
+
+    private  String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private  void uploadImage(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Posting");
+        progressDialog.show();
+
+        if(imageUri != null){
+            final StorageReference filereference = storageReference.child(System.currentTimeMillis()
+                    + "." +getFileExtension(imageUri));
+
+            uploadTask = filereference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if(!task.isComplete()){
+                        throw task.getException();
+                    }
+                    return  filereference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        myUrl = downloadUri.toString();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("donates");
+
+                        String donateid = reference.push().getKey();
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("donateid", donateid);
+                        hashMap.put("posImage", myUrl);
+                        hashMap.put("title", Title.getText().toString());
+                        hashMap.put("donater", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                        reference.child(donateid).setValue(hashMap);
+
+                        progressDialog.dismiss();
+
+                        startActivity(new Intent(AddDonationActivity.this, MainActivity.class));
+                        finish();
+                    }else{
+                        Toast.makeText(AddDonationActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AddDonationActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            Toast.makeText(AddDonationActivity.this, "No Image Selected!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //ctrl + o
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            image_added.setImageURI(imageUri);
+        }
+        else
+        {
+            Toast.makeText(this,"Something goes wrong", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(AddDonationActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+}
