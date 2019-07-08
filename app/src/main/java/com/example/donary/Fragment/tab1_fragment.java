3a7043c1bbcdb8eb3a383b7a8bc8a01108e89b31
Tenking <keesh-wa17@student.tarc.edu.my>
2019-07-08 22:34:04 +0800

    bottom navigation

--- app/src/main/java/com/example/donary/Fragment/tab1_fragment.java
+++ app/src/main/java/com/example/donary/Fragment/tab1_fragment.java
@@ -1,4 +1,4 @@
-package com.example.donary;
+package com.example.donary.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,16 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.donary.CreateEvent;
+import com.example.donary.R;
 import com.example.donary.adapters.AdapterEventPost;
 import com.example.donary.models.ModelEventPost;
-import com.facebook.login.LoginManager;
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
